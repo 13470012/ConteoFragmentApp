6de4122b16b5641ec7c73063a6cc23f0abd144b2
jose.can <jose.can@itrends.com.mx>
2017-10-27 11:54:28 -0500

    Proyecto ConteoFragmentApp. Agregando interactividad de usuario, manejando los eventos

--- app/src/main/java/com/josecan/android/conteofragmentapp/ConteoFragment.java
+++ app/src/main/java/com/josecan/android/conteofragmentapp/ConteoFragment.java
@@ -6,12 +6,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by jose.can on 27/10/2017.
  */
 
 public class ConteoFragment extends Fragment {
+    int conteo = 0;
+    EditText conteoEditText;
+    Button contadorButton;
+    Button reiniciarButton;
+
+
     public ConteoFragment() {
         super();
     }
@@ -25,6 +33,26 @@ public class ConteoFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_conteo, container, false);
+
+        conteoEditText = (EditText) v.findViewById(R.id.conteo_edit_text);
+        contadorButton = (Button) v.findViewById(R.id.contador_button);
+        reiniciarButton = (Button) v.findViewById(R.id.reiniciar_button);
+
+        contadorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                conteo ++;
+                conteoEditText.setText(Integer.toString(conteo));
+            }
+        });
+
+        reiniciarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                conteo = 0;
+                conteoEditText.setText(Integer.toString(conteo));
+            }
+        });
 
         return v;
     }
