43e8c5261a0329e9e259b5a70a3353a7ff108787
jose.can <jose.can@itrends.com.mx>
2017-10-27 11:46:57 -0500

    Version inicial del proyecto ConteoFragmentApp. No incluye manejo de eventos.

---
+++ app/src/main/java/com/josecan/android/conteofragmentapp/ConteoFragment.java
@@ -0,0 +1,31 @@
+package com.josecan.android.conteofragmentapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by jose.can on 27/10/2017.
+ */
+
+public class ConteoFragment extends Fragment {
+    public ConteoFragment() {
+        super();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_conteo, container, false);
+
+        return v;
+    }
+}
--- app/src/main/java/com/josecan/android/conteofragmentapp/MainActivity.java
+++ app/src/main/java/com/josecan/android/conteofragmentapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.josecan.android.conteofragmentapp;
 
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,5 +12,12 @@ public class MainActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+        if (fragment == null){
+            fragment = new ConteoFragment();
+            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
+        }
     }
 }
